bd46963eb84852c7104080882ed3d24e85a6affe
Pavel <pavelb73@gmail.com>
2024-10-18 20:44:49 +0300

    prepare to deploy

--- MainApp/admin.py
+++ MainApp/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
+from MainApp.models import Snippet, Comment
+
+
 # Register your models here.
+
+# 1 variant
+# admin.site.register(Snippet)
+# admin.site.register(Comment)
+
+# 2 variant
+@admin.register(Snippet)
+class SnippetAdmin(admin.ModelAdmin):
+    list_display = ['name', 'lang', 'code', 'public']
+    list_filter = ['lang', 'creation_date']
+    
+    search_fields = ['name', 'code']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['snippet', 'text', 'creation_date', 'author']
+     
+    ordering = ['creation_date', 'author'] 
+    
+    search_fields = ['text']
